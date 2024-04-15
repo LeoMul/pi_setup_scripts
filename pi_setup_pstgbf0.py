b7c9ca823eb3db87c125056e0e955e4dc41938cc
Leo Mulholland <leomulholland@Leos-MacBook-Pro.local>
2024-04-15 22:34:33 +0100

    couple of bug fixes, refactor

--- pi_setup_pstgbf0.py
+++ pi_setup_pstgbf0.py
@@ -1,20 +1,49 @@
-import numpy as np 
 import sys
 import os 
-#notes
+import json 
+import argparse
+
+#sets up and (optionally) runs your pstgbf0 jobs.
+
+#it 
+
 #i am assuming that only one symmetry per elev file for now.
 
+#todo - make better use of the oop. edge cases 
+
+#example - the code right now produces all the possible allowed final states from the initial bound state.
+#I should ask Connor if this is sound - or if the possible allowed final states should be limited to those in 
+#the DSTG2.INP or the sizeBP files.
+
+
+
+default_pstgbf0_exec_path = 'pstgbf0.x'
+
+
+#default stuff so the user knows how to easily change the input.
 default_email = '40268323@ads.qub.ac.uk'
 default_partition = 'k2-math-physics'
 default_time_limit = '1:00:00'
-default_directory_list_location = ''
+
+default_run_jobs = True
+
+default_hamiltonian_path = 'H.DAT'
+default_D_files_path = '../D*'
+default_bound_state_directory_abs_path = ''
+
+default_directory_list = ['symdir_2j1_p0','symdir_2j0_p1']
+
 default_mem_per_cpu = 6
 default_nodes = 1
 default_num_cpu_per_node = 128
 
+#these are in Rydbergs. NOT scaled Rydbergs. THe code does that for you.
 default_E0 = 0.01
 default_NE = 128
 default_DE = 0.1
+
+#this assumes you correctly set the guage in stgd. 
+#I'm looking at you, past Leo, you rapscallion.
 default_guage = 'len'
 
 class Input:
@@ -25,11 +54,16 @@ class Input:
                  mem_per_cpu_GB = default_mem_per_cpu,
                  nodes=default_nodes,
                  cpu_per_node = default_num_cpu_per_node,
+                 run_jobs = default_run_jobs,
                  e0_ryd=default_E0,
                  num_e_points=default_NE,
                  delta_e_ryd = default_DE,
                  gauge = default_guage,
-                 location_of_directory_list = default_directory_list_location):
+                 directory_list = default_directory_list,
+                 hamiltonian_absolute_path = default_hamiltonian_path,
+                 D_files_absolute_path = default_D_files_path,
+                 bound_state_directory_abs_path = default_bound_state_directory_abs_path,
+                 pstgbfx_absolute_path = default_pstgbf0_exec_path):
  
         self.email = email 
         self.partition = partition
@@ -37,11 +71,16 @@ class Input:
         self.cpu_per_node = cpu_per_node
         self.mem_per_cpu_GB = mem_per_cpu_GB
         self.nodes= nodes
+        self.run_jobs = run_jobs
         self.e0_ryd = e0_ryd
         self.num_e_points = num_e_points
         self.delta_e_ryd = delta_e_ryd
         self.gauge = gauge
-        self.location_of_directory_list = location_of_directory_list
+        self.pstgbfx_absolute_path = pstgbfx_absolute_path
+        self.hamiltonian_absolute_path = hamiltonian_absolute_path
+        self.D_files_absolute_path = D_files_absolute_path
+        self.bound_state_directory_abs_path = bound_state_directory_abs_path
+        self.directory_list = directory_list
 
 
 
@@ -115,102 +154,103 @@ def write_dstgbf0damp(final_bound_states,num_bound_states,num_points,e0,de,guage
 
 def write_pstgbf0_job_script(partition,nodes,cpu_per_node,time_limit,mem_gb,email,title):
 
+    current_directory = os.getcwd()
+
     total_cpu = nodes * cpu_per_node
 
-    stgb_job = open('pstgbf.job','w')
+    pstgbf0_job = open('pstgbf.job','w')
+    pstgbf0_job.write('#!/bin/bash\n')
+    pstgbf0_job.write('#SBATCH --job-name={}\n'.format(title))
+    pstgbf0_job.write('#SBATCH --nodes={}\n'.format(nodes))
+    pstgbf0_job.write('#SBATCH --ntasks-per-node={}\n'.format(cpu_per_node))
+    pstgbf0_job.write('#SBATCH --mem-per-cpu={}G\n'.format(mem_gb) )
+    pstgbf0_job.write('#SBATCH --time={}\n'.format(time_limit))
+    pstgbf0_job.write('#SBATCH --partition={} \n'.format(partition))
+    pstgbf0_job.write('#SBATCH --error="error_stgb.out"\n')
+    pstgbf0_job.write('#SBATCH --mail-type=BEGIN,FAIL,END\n')
+    pstgbf0_job.write('#SBATCH --mail-user={}\n'.format(email))
+    pstgbf0_job.write('SCRATCH_DIRECTORY=/mnt/scratch2/users/${USER}/${SLURM_JOBID}.pex  ## scratch2 has 2PB size; no quota^M\n')
+    pstgbf0_job.write('export TMPDIR={}\n'.format(current_directory))
+    pstgbf0_job.write('cd {}\n'.format(current_directory))
+    pstgbf0_job.write('module purge\n')
+    pstgbf0_job.write('module load mpi/openmpi/4.0.0/gcc-5.1.0\n')
+    pstgbf0_job.write('module load compilers/gcc/9.3.0\n')
+    pstgbf0_job.write('mpirun -np {} ./pstgbf0.x'.format(total_cpu))
+    pstgbf0_job.close()
 
-    stgb_job.write('#!/bin/bash\n')
+def run_many_pstgf(input:Input):
+    directories = input.directory_list
+    num_points = input.num_e_points
+    e0=input.e0_ryd
+    de=input.delta_e_ryd
+    partition=input.partition
+    nodes=input.nodes
+    cpu_per_node=input.cpu_per_node
+    time_limit=input.time_limit
+    mem_gb=input.mem_per_cpu_GB
+    email = input.email
+    guage = input.gauge
 
-    stgb_job.write('#SBATCH --job-name={}\n'.format(title))
+    bound_path = input.bound_state_directory_abs_path
 
-    stgb_job.write('#SBATCH --nodes={}\n'.format(nodes))
-    stgb_job.write('#SBATCH --ntasks-per-node={}\n'.format(cpu_per_node))
-    stgb_job.write('#SBATCH --mem-per-cpu={}G\n'.format(mem_gb) )
-    stgb_job.write('#SBATCH --time={}\n'.format(time_limit))
-    stgb_job.write('#SBATCH --partition={} \n'.format(partition))
-    stgb_job.write('#SBATCH --error="error_stgb.out"\n')
-    stgb_job.write('#SBATCH --mail-type=BEGIN,FAIL,END\n')
-    stgb_job.write('#SBATCH --mail-user={}\n'.format(email))
-    stgb_job.write('SCRATCH_DIRECTORY=/mnt/scratch2/users/${USER}/${SLURM_JOBID}.pex  ## scratch2 has 2PB size; no quota^M\n')
-
-    stgb_job.write('module purge\n')
-    stgb_job.write('module load mpi/openmpi/4.0.0/gcc-5.1.0\n')
-    stgb_job.write('module load compilers/gcc/9.3.0\n')
-
-    stgb_job.write('mpirun -np {} ./pstgbf0.x'.format(total_cpu))
-
-    stgb_job.close()
-
-def run_many_pstgf(directories,eff_charge,num_points,e0,de,partition,nodes,cpu_per_node,time_limit,mem_gb,email,guage):
-
+    d_path = input.D_files_absolute_path
+    ham_path = input.hamiltonian_absolute_path
+    run_jobs = input.run_jobs
     e0_new = e0 
     de_new = de
 
+    if guage =='len':
+        guage =0
+    elif guage == 'vel':
+        guage =1
+
     print(directories)
+
     for directory in directories:
         print('changing directory to,',directory)
         os.chdir(directory)
-        os.system('cp ../pstgbf0.x .')
+        os.system('cp {} .'.format(input.pstgbfx_absolute_path))
         #os.system('cd {}'.format(directory))
         
-        initial_symmetry,num_bound_states,Z,N = read_elev('ELEV')
-        eff = Z - N 
-        print('found',Z,N)
-        if eff != 0:
-            e0_new = e0 / eff**2 
-            de_new = de /eff ** 2 
-        
+        if os.path.exists('ELEV'):
+            
+            bound_symmetry_path = bound_path+'/'+directory
 
-        new_symmetries = determine_final_bound_states(initial_symmetry)
-        write_dstgbf0damp(new_symmetries,num_bound_states,num_points,e0_new,de_new,guage)
-        title = str(''.join(initial_symmetry))+'bf'
-        print(title)
-        write_pstgbf0_job_script(partition,nodes,cpu_per_node,time_limit,mem_gb,email,title)
-        os.system('sbatch pstgbf.job')
+            initial_symmetry,num_bound_states,Z,N = read_elev(bound_symmetry_path+'/ELEV')
+
+            if num_bound_states != 0:
+
+                eff = Z - N 
+                print('found nuclear charge {} and num electrons {}'.format(Z,N))
+                if eff != 0:
+                    e0_new = e0 / eff**2 
+                    de_new = de /eff ** 2 
+
+
+                new_symmetries = determine_final_bound_states(initial_symmetry)
+                write_dstgbf0damp(new_symmetries,num_bound_states,num_points,e0_new,de_new,guage)
+                title = str(Z)+str(N)+str(''.join(initial_symmetry))+'bf'
+                print('making job script with title',title)
+                write_pstgbf0_job_script(partition,nodes,cpu_per_node,time_limit,mem_gb,email,title)
+                os.system('ln -s {} H.DAT'.format(ham_path))
+                os.system('ln -s {}/B* .'.format(bound_symmetry_path))
+
+                os.system('ln -s {} .'.format(d_path))
+                if run_jobs:
+                    os.system('sbatch pstgbf.job')
+            else:
+                print(' i found no bound states, skipping sym',directory)
+        else:
+            print('i did not find an ELEV in',directory,' so i am skipping it.')
+        print('*************')
         os.chdir('..')
 
 def main(input:Input):
 
-    num_points = input.num_e_points
-    e0 = input.e0_ryd
-    d0 = input.delta_e_ryd
-
-    guage = input.gauge #options: 'len','vel'
-
-    partition = input.partition
-    time_limit = input.time_limit
-    email = input.email
-    nodes = input.nodes
-    num_cpu_per_node = input.cpu_per_node
-    mem_per_cpu = input.mem_per_cpu_GB
-
-    if guage =='len':
-        guage_key =0
-    elif guage == 'vel':
-        guage_key =1
-
-    eff = 1
-
-    f = open(input.location_of_directory_list,'r')
-    f_read = f.readlines()
-    f.close()
-    directories_raw = []
-    for line in f_read:
-        linelist = list(line)
-        if not('#' in linelist):
-            directories_raw.append(line.replace('\n',''))
-
-    #directories_raw = [(np.loadtxt('directories_for_pstgbf',dtype=str))]
-    
-    print('found directories: ',directories_raw)
-    run_many_pstgf(directories_raw,eff,num_points,e0,d0,partition,nodes,num_cpu_per_node,time_limit,mem_per_cpu,email,guage_key)
+    run_many_pstgf(input)
 
     
     return 0 
-
-
-import json 
-import argparse
 
 
 parser = argparse.ArgumentParser()
--- pi_setup_stgb.py
+++ pi_setup_stgb.py
@@ -1,47 +1,102 @@
 import os 
-import numpy as np 
+import json 
+import argparse
 
+#sets up and (optionally) runs your bound state calculations for photoionisation.
+
+#todo:
+#refactor the functions so they actually use the object orientation.
+#rn the OOP is only for easy transmission of the input. most everything else is still entirely functionally 
+#programmed which seems wasteful when there is OOP in here anyway.  
+
+#refactor on 15.04.24
+#stgb only needs the Hamiltonian. For this reason I'll move the sym linking of the D files to the
+#pstgb set up code, and there change it so the user gives the path to the B files which are then 
+#sym linked.
+
+#additionally - change a separate symmetry deck to a list of symmetries in the input deck.
+#makes the input deck entirely self contained.
+
+#also - you need to send the code to Connor after you do this 
+#- you from the past
+
+
+#this is hard coded right now. i might change that.
+stgb_exec_path = '../stgb_blas.proto.x'
+
+#what does the input deck do, Leo? 
+
+#overwrite_existing_arrays - boolean - whether or not this code overwrites directories it finds with the same name as it calcualtes.
+#symmetries - list of strings in format "2j parity".
+#hamiltonian_path -str- - real (or relative) path of the Hamiltonian.dat
+#email - str -email for slurm - leaving it blank should in theory stop it. (i havent tested that)
+#partition - str -  slurm partition
+#time_limit - str -slurm job time limit
+#mem_per_cpu_GB - int -memory per cpu in GB
+#run_jobs - boolean - whether or not the code sbatch's the jobs made. 
+#for testing really, afterwards one might type sbatch symdir*/stgb.job  or the specific ones they want.
+
+#n0,nf,dn refer to the quantum defect paramters for stgb. see the code.
+
+
+#default stuff, so that the user easily knows how to change the input. in theory.
+
+#the user might consider changing the defaults to suit their needs.
 default_email = '40268323@ads.qub.ac.uk'
-default_partition = 'k2-math-physics'
+default_partition = 'k2-math-physics-debug'
 default_time_limit = '1:00:00'
 default_mem_per_cpu = 6
 
-default_symmetry_path = ''
-default_directory_of_data = '..'
+default_symmetries = ['1 0','1 1']
+default_hamiltonian_path = '../H.DAT'
 
 default_n0 = 0.21
-default_n = 10.2
+default_n = 4.2
 default_dn = 0.2
 
 default_rewrite_override = True
 
+default_run_jobs = True
+
 class Input:
+    #this object oriented business allows the input deck to be read in in the json format
+    #which makes my life a bit easier.
     def __init__(self,
-                 override=default_rewrite_override,
-                 symmetry_path=default_symmetry_path,
-                 data_directory=default_directory_of_data,
+                 overwrite_existing_arrays=default_rewrite_override,
+                 symmetries=default_symmetries,
+                 hamiltonian_path=default_hamiltonian_path,
                  email=default_email,
                  partition=default_partition,
                  time_limit = default_time_limit,
-                 mem_per_cpu = default_mem_per_cpu,
+                 mem_per_cpu_GB = default_mem_per_cpu,
+                 run_jobs = default_run_jobs,
                  n0=default_n0,
                  nf=default_n,
                  dn=default_dn):
-        self.override = override
-        self.symmetry_path = symmetry_path 
-        self.data_directory = data_directory
+        self.overwrite_existing_arrays = overwrite_existing_arrays
+        self.symmetries = symmetries 
+        self.hamiltonian_path = hamiltonian_path
         self.email = email 
         self.partition = partition
         self.time_limit = time_limit
-        self.mem_per_cpu = mem_per_cpu
-        self.n0 = default_n0
+        self.mem_per_cpu_GB = mem_per_cpu_GB
+        self.run_jobs = run_jobs
+        self.n0 = n0
         self.nf = nf 
         self.dn = dn
 
 
-def read_in_symmetries(path_to_symmetries):
-    #reads in the symmetries file
-    symmetries = np.loadtxt(path_to_symmetries,dtype=str)
+def read_in_symmetries(input:Input):
+    #reads in the symmetries in in the input deck, returns aa list[list[str]]
+    symmetries_strings_list = input.symmetries
+    symmetries = []
+    for sym in symmetries_strings_list:
+
+        split = sym.split()
+        twoj = (split[0])
+        pari = (split[1])
+
+        symmetries.append([twoj,pari])
     return symmetries
 
 def decode_symmetries(symmetries):
@@ -49,21 +104,21 @@ def decode_symmetries(symmetries):
     desired_symmetries = []
     
     for row in symmetries:
-        string = 'symdir_2j'+row[0] + '_p' + row[1]
+        string = 'symdir_2j'+str(row[0]) + '_p' + str(row[1])
         desired_symmetries.append(string)
 
     return desired_symmetries
 
 def make_symmetry_directories(desired_directories,symmetries):
 
-    #makes the directories - ignores ones that already exists.
+    #makes the directories - ignores ones that already exists - UNLESS override is on.
     #ouputs the directories that didnt already exist - these are the ones that we need to consider.
     #directories = desired_directories.copy()
     directories_kept = []
     symmetries_kept = []
     for (index,directory) in enumerate(desired_directories):
         if os.path.isdir(directory):
-            print(directory,' already exists - skipping')
+            print(directory,' already exists WARNING')
         else:
             os.mkdir(directory)
             symmetries_kept.append(symmetries[index])
@@ -74,7 +129,9 @@ def make_symmetry_directories(desired_directories,symmetries):
     return directories_kept,symmetries_kept
 
 def write_dstgb(symmetry,n0,nf,delta_n):
-    #writes dstgb.
+    
+    #writes dstgb, based on the parameters you give it. 
+    #you, the user, might consider changing some of it.
 
 
     symmetry_string = ' {} {} {}\n'.format(0,symmetry[0],symmetry[1])
@@ -91,6 +148,8 @@ def write_dstgb(symmetry,n0,nf,delta_n):
 
 def write_stgb_job_script(partition,time_limit,mem_gb,email,title):
 
+    current_directory = os.getcwd()
+
     stgb_job = open('stgb.job','w')
 
     stgb_job.write('#!/bin/bash\n')
@@ -105,7 +164,9 @@ def write_stgb_job_script(partition,time_limit,mem_gb,email,title):
     stgb_job.write('#SBATCH --error="error_stgb.out"\n')
     stgb_job.write('#SBATCH --mail-type=BEGIN,FAIL,END\n')
     stgb_job.write('#SBATCH --mail-user={}\n'.format(email))
-    stgb_job.write('SCRATCH_DIRECTORY=/mnt/scratch2/users/${USER}/${SLURM_JOBID}.pex  ## scratch2 has 2PB size; no quota^M\n')
+    stgb_job.write('SCRATCH_DIRECTORY=/mnt/scratch2/users/${USER}/${SLURM_JOBID}.pex  \n')
+    stgb_job.write('export TMPDIR={}\n'.format(current_directory))
+    stgb_job.write('cd {}\n'.format(current_directory))
 
     stgb_job.write('module purge\n')
     stgb_job.write('module load mpi/openmpi/4.0.0/gcc-5.1.0\n')
@@ -116,23 +177,26 @@ def write_stgb_job_script(partition,time_limit,mem_gb,email,title):
     stgb_job.close()
 
 
-def setup_many_dstgb_directories(directories,symmetries,n0,nf,delta_n,partition,time_limit,mem_gb,email,title,data_directory):
+def setup_many_dstgb_directories(directories,symmetries,n0,nf,delta_n,partition,time_limit,mem_gb,email,title_end,hamiltonian_path,run_jobs):
 
     assert(len(directories) == len(symmetries))
 
-    print('linking data from directory:',data_directory)
 
     for ii in range(0,len(directories)):
         os.chdir(directories[ii])
         write_dstgb(symmetries[ii],n0,nf,delta_n)
-        #print(symmetries[ii])
-        title = ''.join(symmetries[ii])+'stgb'
-        print(title)
+        title = ''.join(symmetries[ii])+title_end
+
+        print('writing job script with title ',title)
+        #write out a stgb job script
         write_stgb_job_script(partition,time_limit,mem_gb,email,title)
-        os.system('cp ../stgb_blas.proto.x .')
-        os.system('sbatch stgb.job')
-        os.system('ln -s ../{}/D* .'.format(data_directory))
-        os.system('ln -s ../{}/H.DAT H.DAT'.format(data_directory))
+
+        os.system('cp {}  .'.format(stgb_exec_path))
+        os.system('ln -s {} H.DAT'.format(hamiltonian_path))
+        print('linking hamiltonian from directory:',hamiltonian_path)
+
+        if run_jobs:
+            os.system('sbatch stgb.job')
 
         os.chdir('..')
 
@@ -147,30 +211,29 @@ def main(input:Input):
     partition = input.partition
     time_limit = input.time_limit
     title = 'stgb'
-    mem_gb = input.mem_per_cpu
+    mem_gb = input.mem_per_cpu_GB
 
-    directory_of_data_files = input.data_directory
+    directory_of_data_files = input.hamiltonian_path
 
-    symmetries = read_in_symmetries(input.symmetry_path)
+    symmetries = read_in_symmetries(input)
     desired_directories = decode_symmetries(symmetries)
 
     print('the desired directories are:',desired_directories)
 
-    over_ride = input.override
+    over_ride = input.overwrite_existing_arrays
     directories_to_be_worked_with,symmetries_kept = make_symmetry_directories(desired_directories,symmetries)
 
     #if the user chooses to override, the directories will be entered anyway and the various stgb's written.
     if over_ride:
+        print('override is on - so i will overwrite the directories that already exist')
         directories_to_be_worked_with = desired_directories
         symmetries_kept = symmetries
 
     print('working in directories: ',directories_to_be_worked_with)
     print('working with symmetries: ',symmetries_kept)
+    run_jobs = input.run_jobs
+    setup_many_dstgb_directories(directories_to_be_worked_with,symmetries_kept,n_0,n_f,delta_n,partition,time_limit,mem_gb,email,title,directory_of_data_files,run_jobs)
 
-    setup_many_dstgb_directories(directories_to_be_worked_with,symmetries_kept,n_0,n_f,delta_n,partition,time_limit,mem_gb,email,title,directory_of_data_files)
-
-import json 
-import argparse
 
 
 parser = argparse.ArgumentParser()
